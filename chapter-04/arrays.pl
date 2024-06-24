274ec7b1e7ebacf3d275c7d327b577f01e5586d1
saudz <saud.zahir@soco-engineers.com>
2024-06-24 18:03:56 +0500

    Initial commit

---
+++ chapter-04/arrays.pl
@@ -0,0 +1,63 @@
+@array = ("a", "e", "i", "o", "u");
+print "@array\n";
+
+
+print "Using 'while' repetition ...\n";
+
+$i = 0;
+while ($i < scalar(@array)) {
+    print "array[$i] = $array[$i]\n";
+    ++$i;
+}
+
+print "\nUsing 'for' repetition ...\n";
+
+for ($i = 0; $i < scalar(@array); ++$i) {
+    print "array[$i] = $array[$i]\n";
+}
+
+# Arrays Function
+
+# Operator qw
+@os = qw (apple windows linux); # ("apple", "windows", "linux")
+print @os, "\n";
+
+# Operator .. (range)
+@range = (1..5);
+print @range, "\n";
+
+@alphabets = ("a".."z");
+print @alphabets, "\n";
+
+# Length of array
+print "Length of array: ", scalar(@alphabets), "\n";
+
+# Last Index of array:
+print "Last index of array: $#range\n";
+
+# Push extends the array/list
+push (@range, (1, 3, 5, 7));
+print @range, "\n";
+
+# Pop removes the last element from the list
+pop (@range);
+print @range, "\n";
+
+# Unshift inserts an element at the front of an array
+unshift @range, "a";
+print @range, "\n";
+
+# Shift removes and returns first element of an array
+$a = shift @range;
+print $a, "\n";
+
+# Splice removes/replaces slices of an array
+# splice (target_array, offset, length, source_array)
+# offset is the index of first element to be modified
+# length if the slice to modify
+# source_array, (if omitted the selected chunk is removed)
+splice @alphabets, 2, 5;
+print @alphabets, "\n";
+
+splice @alphabets, 2, 5, @range;
+print @alphabets, "\n";
