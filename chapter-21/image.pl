944cfb816284c8cc146b5dd04a91c7f6b6bf7a5a
saudz <saud.zahir@soco-engineers.com>
2024-07-18 15:19:20 +0500

    Add graphics

--- chapter-21/image.pl
+++ chapter-21/image.pl
@@ -2,29 +2,48 @@
 
 # Author: Saud Zahir
 # Contact: m.saud.zahir@gmail.com
-# Date: July 3, 2024
+# Date: July 5, 2024
 
 use strict;
 use warnings;
 use GD;
 
+# Create a  new GD image object
+# 320 pixels wide and 320 pixels high
 my $image = new GD::Image( 320, 320 );
 
 my $white = $image->colorAllocate( 255, 255, 255 );
-my $green = $image->colorAllocate( 96, 181, 21 );
-my $skyblue = $image->colorAllocate( 34, 166, 241 );
-my $blue = $image->colorAllocate( 0, 116, 182 );
-my $red = $image->colorAllocate( 223, 55, 43 );
-my $yellow = $image->colorAllocate( 251, 193, 22 );
-my $lightblue = $image->colorAllocate( 26, 115, 232 );
-my $darkgreen = $image->colorAllocate( 37, 151, 69 );
+my $red = $image->colorAllocate( 255, 0, 0 );
+my $green = $image->colorAllocate( 0, 255, 0 );
+my $blue = $image->colorAllocate( 0, 0, 255 );
+my $black = $image->colorAllocate( 0,0,0 );
 
-my $lightgreen = $image->colorAllocate( 120, 188, 36 );
+my $purple = $image->colorAllocate( 255, 0, 255 );
 
-$image->filledRectangle( 15, 15, 300, 300, $blue );
+$image->filledRectangle ( 15, 15, 150, 150, $red );
+$image->arc( 200, 200, 50, 50, 0, 360, $black );
+$image->fill( 200, 200, $blue );
+$image->rectangle ( 100, 100, 200, 125, $green );
+$image->fillToBorder( 150, 110, $green, $green );
 
-open( PCIT, ">image.png" ) or die( "Can not open picture: $!" );
+my $polygon = new GD::Polygon();
+$polygon->addPt (20, 300 );
 
-binmode( PCIT );
-print( PCIT $image->png() );
-close( PCIT ) or die( "Can not close picture: $!" );
+$polygon->addPt( 20, 175 );
+$polygon->addPt( 100, 175 );
+
+$image->polygon( $polygon, $blue );
+$image->fill ( 50, 200, $purple );
+
+$polygon->setPt( 0, 30, 300 );
+$polygon->setPt( 1, 110, 300 );
+$polygon->setPt( 2, 110, 175);
+
+$image->filledPolygon( $polygon, $black );
+
+open ( PICT, ">image.png" ) or
+    die( "Can not open picture: $!" );
+
+binmode ( PICT );
+print ( PICT $image->png() );
+close ( PICT ) or die( "Can not close file: $!" );
